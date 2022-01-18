9ec88d51a8e0b5ab323849e90d2eca2218c1863b
Evan Irving-Pease <evan.irvingpease@gmail.com>
2022-01-18 13:29:22 +0000

    updates to the IGSR population metadata conversion

--- scripts/convert_igsr_populations.R
+++ scripts/convert_igsr_populations.R
@@ -40,13 +40,14 @@ meta <- read_tsv(argv$igsr, col_types = cols(.default = "c")) %>%
 super_codes <- list(
   "African Ancestry" = "AFR",
   "American Ancestry" = "AMR",
-  "Central Asian Ancestry" = "CAS",
+  "Central Asian and Siberian Ancestry" = "CAS",
+  "Central and South Asian Ancestry" = "CSA",
   "East Asian Ancestry" = "EAS",
   "European Ancestry" = "EUR",
   "Middle Eastern Ancestry" = "MEA",
   "Oceanian Ancestry" = "OCE",
   "South Asian Ancestry" = "SAS",
-  "West Asian Ancestry" = "WAS"
+  "West Eurasian Ancestry" = "WEA"
 )
 
 # map the HGDP and SGDP super-populations onto the 1000G names
@@ -55,7 +56,7 @@ super_map <- list(
   # HGDP
   "Africa (HGDP)" = "African Ancestry",
   "America (HGDP)" = "American Ancestry",
-  "Central South Asia (HGDP)" = "South Asian Ancestry",
+  "Central South Asia (HGDP)" = "Central and South Asian Ancestry",
   "East Asia (HGDP)" = "East Asian Ancestry",
   "Europe (HGDP)" = "European Ancestry",
   "Middle East (HGDP)" = "Middle Eastern Ancestry",
@@ -64,47 +65,25 @@ super_map <- list(
   # SGDP
   "Africa (SGDP)" = "African Ancestry",
   "America (SGDP)" = "American Ancestry",
-  "Central Asia and Siberia (SGDP)" = "Central Asian Ancestry",
+  "Central Asia and Siberia (SGDP)" = "Central Asian and Siberian Ancestry",
   "East Asia (SGDP)" = "East Asian Ancestry",
   "Oceania (SGDP)" = "Oceanian Ancestry",
   "South Asia (SGDP)" = "South Asian Ancestry",
-  "West Eurasia (SGDP)" = "European Ancestry"
-)
-
-
-# manual override of some super population categories (mostly SGDP)
-pop_map <- list(
-  # Middle Eastern Ancestry
-  "Druze" = "Middle Eastern Ancestry",
-  "Mozabite" = "Middle Eastern Ancestry",
-  "Palestinian" = "Middle Eastern Ancestry",
-  "Iraqi Jew" = "Middle Eastern Ancestry",
-  "Bedouin B" = "Middle Eastern Ancestry",
-  "Druze" = "Middle Eastern Ancestry",
-  "Iranian" = "Middle Eastern Ancestry",
-  "Palestinian" = "Middle Eastern Ancestry",
-  "Yemenite Jew" = "Middle Eastern Ancestry",
-  "Jordanian" = "Middle Eastern Ancestry",
-  "Samaritan" = "Middle Eastern Ancestry",
-
-  # Central Asian Ancestry
-  "Uygur" = "Central Asian Ancestry",
-  "Yakut" = "Central Asian Ancestry",
-  "Tajikistan" = "Central Asian Ancestry",
-
-  # West Asian Ancestry
-  "Armenian" = "West Asian Ancestry",
-  "Turkish" = "West Asian Ancestry",
-  "Abkhazia" = "West Asian Ancestry",
-  "Georgia" = "West Asian Ancestry",
-
-  # East Asian Ancestry
-  "Igorot" = "East Asian Ancestry",
-  "Dusun" = "East Asian Ancestry"
+  "West Eurasia (SGDP)" = "West Eurasian Ancestry"
 )
 
 # create 3-letter population codes based on the population names
 new_codes <- abbreviate(sort(unique(meta$population_name)), minlength = 3, strict = TRUE) %>% toupper()
+
+# old_codes[old_codes == "ESN"]
+# new_codes[new_codes == "ESN"]
+
+# manual override of some duplicate codes
+new_codes["Aleut"] <- "ALE"
+new_codes["Brahui"] <- "BRA"
+new_codes["Eskimo Naukan"] <- "ESK"
+new_codes["Mongola"] <- "MGA"
+new_codes["Palestinian"] <- "PAL"
 
 # get the codes that have already been defined
 old_codes <- meta %>%
@@ -133,10 +112,9 @@ dup <- meta %>%
   unique() %>%
   group_by(population) %>%
   tally() %>%
-  filter(n > 1) %>%
-  pull(population)
+  filter(n > 1)
 
-stopifnot(length(dup) == 0)
+stopifnot(nrow(dup) == 0)
 
 # check the pairings are consistent
 dup <- meta %>%
@@ -144,62 +122,37 @@ dup <- meta %>%
   unique() %>%
   group_by(population_name) %>%
   tally() %>%
-  filter(n > 1) %>%
-  pull(population_name)
+  filter(n > 1)
 
-stopifnot(length(dup) == 0)
-
-# manual override of some super population categories (mostly SGDP)
-meta <- meta %>%
-  mutate(superpopulation_name = ifelse(population_name %in% names(pop_map), as.character(pop_map[population_name]), superpopulation_name))
+stopifnot(nrow(dup) == 0)
 
 # apply the super-population name and code mapping
 meta <- meta %>%
   mutate(superpopulation_name = ifelse(superpopulation_name %in% names(super_map), as.character(super_map[superpopulation_name]), superpopulation_name)) %>%
   mutate(superpopulation = ifelse(superpopulation_name %in% names(super_codes), as.character(super_codes[superpopulation_name]), superpopulation))
 
-# check the pairings are consistent
+# some populations exist in multiple sources, so prefer 1000G -> HGDP -> SGDP for the metadata
+prefer_source <- function(pops) {
+  unlist(lapply(pops, function(pop) {
+    1 + str_detect(pop, "HGDP") * 2 + str_detect(pop, "SGDP") * 3
+  }))
+}
+
+# only keep one entry per population
+meta <- meta %>%
+  group_by(population_name) %>%
+  slice_min(order_by = prefer_source(population_desc)) %>%
+  mutate(population_desc = str_replace(population_desc, " [(](SGDP|HGDP)[)]", ""))
+
+# check the superpopulation pairings are consistent
 dup <- meta %>%
   select(population_name, superpopulation_name) %>%
   unique() %>%
   group_by(population_name) %>%
   tally() %>%
-  filter(n > 1) %>%
-  pull(population_name)
+  filter(n > 1)
 
-stopifnot(length(dup) == 0)
-
-# convert to spatial object
-meta_sf <- meta %>%
-  select(-population_desc) %>%
-  unique() %>%
-  st_as_sf(coords = c("longitude", "latitude"))
-
-# get a world map
-world <- map_data("world")
-
-# colour mapping
-cols <- c(
-  "African Ancestry" = "#e41a1c",
-  "American Ancestry" = "#377eb8",
-  "Central Asian Ancestry" = "#4daf4a",
-  "East Asian Ancestry" = "#984ea3",
-  "European Ancestry" = "#ff7f00",
-  "Middle Eastern Ancestry" = "#ffff33",
-  "Oceanian Ancestry" = "#a65628",
-  "South Asian Ancestry" = "#f781bf"
-)
-
-ggplot() +
-  geom_map(data = world, map = world, mapping = aes(long, lat, map_id = region)) +
-  geom_sf_label(data = meta_sf, mapping = aes(color = superpopulation_name, label = population_name), cex = 2) +
-  scale_color_manual(values = cols)
-
-# only keep one description per population
-meta <- meta %>%
-  group_by(population_name) %>%
-  slice(1) %>%
-  mutate(population_desc = str_replace(population_desc, " [(](SGDP|HGDP)[)]", ""))
+stopifnot(nrow(dup) == 0)
 
 # save the metadata
 write_tsv(meta, argv$output, na = "")
